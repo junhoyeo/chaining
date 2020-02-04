a4651da7ce5dbbdf494b7a64d777a9f2505c1efb
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-02-05 01:18:51 +0900

    chore(package): add files for deployment

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'chaining',
+  packages = ['chaining'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Package that implements functional chaining in Python, which behaves like JavaScript',
+  author = 'Junho Yeo',
+  author_email = 'hanaro0704@gmail.com',
+  url = 'https://github.com/junhoyeo/chaining',
+  download_url = 'https://github.com/junhoyeo/chaining/archive/v_0.0.1.tar.gz',
+  keywords = [
+    'functional',
+    'chaining',
+  ],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
