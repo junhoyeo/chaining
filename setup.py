e088aacbbef438031456fdc9e387271de2e7d3d6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-02-05 06:08:47 +0900

    chore(package): release v_0.0.2

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
+with open('README.md', 'r') as file_handler:
+  long_description = file_handler.read()
 
-setup(
-  name = 'chaining',
-  packages = ['chaining'],
-  version = '0.0.1',
+setuptools.setup(
+  name='chaining',
+  packages=['chaining'],
+  version='0.0.2',
   license='MIT',
-  description = 'Package that implements functional chaining in Python, which behaves like JavaScript',
+  description='Package that implements functional chaining in Python, which behaves like JavaScript',
   long_description=long_description,
-  author = 'Junho Yeo',
-  author_email = 'hanaro0704@gmail.com',
-  url = 'https://github.com/junhoyeo/chaining',
-  download_url = 'https://github.com/junhoyeo/chaining/archive/v_0.0.1.tar.gz',
-  keywords = [
+  long_description_content_type='text/markdown',
+  author='Junho Yeo',
+  author_email='hanaro0704@gmail.com',
+  url='https://github.com/junhoyeo/chaining',
+  download_url='https://github.com/junhoyeo/chaining/archive/v_0.0.2.tar.gz',
+  keywords=[
     'functional',
     'chaining',
   ],
@@ -28,4 +29,5 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
   ],
+  python_requires='>=3.6',
 )
